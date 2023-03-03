0ddb8af5c32ab3d198eaeff01fb920c95b774043
Liquid Glass <liquid.glass.drops@gmail.com>
2023-03-04 04:34:41 +0800

    Checkpoint (2023 Mar 4)
    Do Not Use
    
    Signed-off-by: Liquid Glass <liquid.glass.drops@gmail.com>

---
+++ py/GenerateWallet.py
@@ -0,0 +1,12 @@
+from algosdk import account, mnemonic
+
+def generate_keypair ():
+	private_key, address = account.generate_account()
+	new_document_path.write("Address: {}\n\n" .format(address))
+	new_document_path.write("Seed: {}" .format(mnemonic.from_private_key(private_key)))
+
+filename = input("Name of File: ")
+new_document_path = open(filename+".txt", "x")
+generate_keypair()
+new_document_path.close()
+print("\n\nPlease Keep The Newly Generated Keypair Safe!\n\nAnyone Who Has Your Seed CAN Spend Your Algos!\n\n")

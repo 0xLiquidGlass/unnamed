26267fb4a8bfd98859515f679c8fec66a5604479
Liquid Glass <liquid.glass.drops@gmail.com>
2023-06-13 16:35:04 +0800

    Checkpoint 2023 June 13
    Preparing GUI on Unnamed Wallet

---
+++ unnamedwallet/src/wip/gui/UnnamedGui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+instance=tk.Tk()
+
+instance.title("Unnamed Wallet (GUI)")
+
+testButton=tk.Button(instance, text="This is a sample")
+
+testButton.pack()
+
+instance.mainloop()
